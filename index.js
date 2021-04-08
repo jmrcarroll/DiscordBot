d5fcf818fa1fc20329f9e15fbcdeeaa6cc7db503
jmrcarroll <carrolljohnmr@gmail.com>
2021-04-08 13:48:37 +0100

    Bot basics - can join and responde to !ping

---
+++ index.js
@@ -0,0 +1,20 @@
+const discord = require('discord.js');
+const dotenv = require("dotenv");
+const client = new discord.Client();
+dotenv.config()
+//console.log(process.env.token);
+console.log("||UNNAMED BOT STARTING||");
+
+
+client.once('ready', ()=>{
+    console.log('UNNAMED BOT READY! BEEP BOOP MOTHERFUCKERS!!');
+});
+
+client.on('message', message=>{
+    console.log(message.author.username + " in " + message.guild.name+" #"+ message.channel.name + ":\t" + message.content)
+    if (message.content == "!ping" && !message.author.bot){
+        message.react("😂")
+        message.channel.send("!ping")
+    }
+});
+client.login(process.env.token);
