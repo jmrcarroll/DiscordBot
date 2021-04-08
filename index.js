9daa94a6d442ac522826da27b039fae9eed94bde
jmrcarroll <carrolljohnmr@gmail.com>
2021-04-08 14:23:48 +0100

    Rewritten to use switch for commands for time being

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const discord = require('discord.js');
 const dotenv = require("dotenv");
 const client = new discord.Client();
+const config = require('./config.json')
+const prefix = config.prefix
 dotenv.config()
 //console.log(process.env.token);
 console.log("||UNNAMED BOT STARTING||");
@@ -12,9 +14,15 @@ client.once('ready', ()=>{
 
 client.on('message', message=>{
     console.log(message.author.username + " in " + message.guild.name+" #"+ message.channel.name + ":\t" + message.content)
-    if (message.content == "!ping" && !message.author.bot){
-        message.react("😂");
-        message.channel.send("!ping");
+    if (message.content.startsWith(prefix) && !message.author.bot){
+        cmd = message.content.substr((prefix.length));
+        switch(cmd.toLowerCase()){
+
+            case "ping": message.channel.send("Pong"); break;
+            case "server": message.channel.send(message.guild.name); break;
+            case "channel": message.channel.send("#" + message.channel.name); break;
+            default: message.channel.send("Unknown command: "+ cmd); break;
+        }
     }
 });
 client.login(process.env.token);
