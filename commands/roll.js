104be4e175386ed2d33632288747f64574896c7a
jmrcarroll <carrolljohnmr@gmail.com>
2021-04-11 19:04:06 +0100

    Made changes to the roll command to revent the bot from overloading upon a request of a roll.

--- commands/roll.js
+++ commands/roll.js
@@ -4,34 +4,53 @@ module.exports = {
     description:"Rolls the specified number of the specified sided dice",
     syntax:"!roll {*Optional:*{Number of dice}d{Number of Sides}} {*Optional:* Modifier(Positive, negitive or Zero)} {*optional*: Descriptor}",
     execute(message, args){
+        console.log("Hello?")
+        mRes = "";
+        var res;
         console.log(args.length);
         if(args.length < 1){
             message.reply("🎲 You rolled 1d6 and got: "+ (Math.floor(Math.random()*6)+1) + " 🎲");
         }else{
             Dice = args[0].toLowerCase().split('d');
             console.log(Dice)
-            res =0;
-            for (i = 0; i < Number(Dice[0]); i++){
-                rand = (Math.floor(Math.random()*Number(Dice[1]))+1);
-                console.log(rand);
-                res = res + rand;
+            console.log(Dice[0])
+            console.log(Dice[1])
+            numDice = parseInt(Dice[0])
+            numSide = parseInt(Dice[1])
+            console.log(numDice<10000)
+            console.log(numSide<10000)
+            if(numDice <10000 && numSide<10000){
+                res =0;
+                for (i = 0; i < numDice; i++){
+                    rand = (Math.floor(Math.random()*numSide)+1);
+                    //console.log(rand);
+                    res = res + rand;
+                }
+            }else{
+                mRes = "Sides and/or number os dice over the limit of 10,000"
+                console.log(mRes)
             }
+            console.log(mRes)
             modifier = 0;
             if(args.length > 1){
-                modifier = Number(args[1]);
+                modifier = parseInt(args[1]);
             }
-            res = res + modifier;
-            mRes = "🎲 You rolled " + Dice[0] + "d" + Dice[1]+" and got: "+ res + " 🎲"
             console.log(mRes)
-            console.log(args)
-            
-            if(args.length > 2 ){
-                mRes = mRes +" Roll was for: "
+            res = res + modifier;
+            if (mRes == ""){
+                mRes = "🎲 You rolled " + Dice[0] + "d" + Dice[1]+" and got: "+ res + " 🎲"
                 console.log(mRes)
-                for (i = 2; i<args.length;i++){
-                    mRes = mRes + " " + args[i]  
+                console.log(args)
+            
+                if(args.length > 2 ){
+                    mRes = mRes +" Roll was for: "
+                    console.log(mRes)
+                    for (i = 2; i<args.length;i++){
+                        mRes = mRes + " " + args[i]  
+                    }
                 }
             }
+            
             message.reply(mRes);
         }
     },
